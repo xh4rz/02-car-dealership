efbc60e1386e167f7f809a0fd0ea53130107bc38
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-11-10 00:37:37 -0500

    37. Inyección de dependencias

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -7,4 +7,13 @@ export class CarsService {
     { id: 2, brand: 'Honda', model: 'Civic' },
     { id: 3, brand: 'Jeep', model: 'Cherokee' },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    return car;
+  }
 }
