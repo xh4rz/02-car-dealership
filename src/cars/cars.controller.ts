60507117fac1147b1d6287e68bf06672e5240dfe
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-11-16 04:00:40 -0500

    45. Interfaces y UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -20,7 +20,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id', ParseIntPipe) id: number) {
+  getCarById(@Param('id') id: string) {
     return this.carsService.findOneById(id);
   }
 
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
-  private cars = [
-    { id: 1, brand: 'Toyota', model: 'Corolla' },
-    { id: 2, brand: 'Honda', model: 'Civic' },
-    { id: 3, brand: 'Jeep', model: 'Cherokee' },
+  private cars: Car[] = [
+    { id: uuid(), brand: 'Toyota', model: 'Corolla' },
+    { id: uuid(), brand: 'Honda', model: 'Civic' },
+    { id: uuid(), brand: 'Jeep', model: 'Cherokee' },
   ];
 
   findAll() {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException(`Car with id ${id} not found`);
