daccccb1335d9f8caa3e1dcb271d0c473e7ee2db
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-11-16 04:07:31 -0500

    46. Pipe - ParseUUIDPipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
@@ -20,7 +21,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: string) {
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
     return this.carsService.findOneById(id);
   }
 
