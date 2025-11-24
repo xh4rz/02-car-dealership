4d6ffd1fbb76a77c8f8674224226c5a7a8d0588b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-11-24 00:34:01 -0500

    52. Actualizar el listado de carros

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -37,7 +37,7 @@ export class CarsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCarDto: UpdateCarDto,
   ) {
-    return updateCarDto;
+    return this.carsService.update(id, updateCarDto);
   }
 
   @Delete(':id')
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,18 @@ async function main() {
 
   app.useGlobalPipes(
     new ValidationPipe({
+      // ! Remueve los datos que van de más en el body de la request
+      // ! Y unicamente envía lo que se ha especificado en el DTO
       whitelist: true,
+
+      // ! Si hay datos de más en el body de la request tira un error
+      // ! Indicando que la propiedad no debería de existir
       forbidNonWhitelisted: true,
+
+      // ! Excluye los campos undefined
+      transformOptions: {
+        exposeUnsetFields: false,
+      },
     }),
   );
 
