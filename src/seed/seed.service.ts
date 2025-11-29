8418c4f7fb87e53626f7f00bd4bd527042815bf7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2025-11-29 03:58:14 -0500

    62. Preparar servicios para insertar SEED

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
   populateDB() {
+    CARS_SEED;
+    BRANDS_SEED;
     return 'SEED executed';
   }
 }
